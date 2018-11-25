39b2768c92e83185615a0fbe82819e7bf51d683a
Henry Till <henrytill@gmail.com>
2018-11-25 17:02:59 -0500

    hecate.cabal: bump QuickCheck upper version bounds

--- tests/Main.hs
+++ tests/Main.hs
@@ -1,14 +1,16 @@
 module Main (main) where
 
-import Control.Monad
-import System.Exit
-import Test.QuickCheck
+import           Control.Monad
+import           System.Exit
+import qualified Test.QuickCheck   as QC
 
-import Hecate.Properties
+import           Hecate.Properties
 
-isSuccess :: Result -> Bool
-isSuccess Success{} = True
-isSuccess _         = False
+
+-- Included for backwards compatibility
+isSuccess :: QC.Result -> Bool
+isSuccess QC.Success{} = True
+isSuccess _            = False
 
 main :: IO ()
 main = do

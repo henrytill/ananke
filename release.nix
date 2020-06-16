118f319bd98820a46d1142de343509c9923ec79a
Henry Till <henrytill@gmail.com>
2020-06-15 20:06:29 -0400

    release.nix: update

--- release.nix
+++ release.nix
@@ -4,19 +4,35 @@ let
   jobs = {
 
     hecate =
-      { compiler ? "ghc862"
+      { compiler ? "ghc883"
       , doCheck ? ! pkgs.stdenv.isDarwin
       }:
 
       let
+        hlint-version       = "2.2.11";
+        lens-family-version = "1.2.3";
+
+        hlint            = pkgs.haskell.packages.${compiler}.callHackage "hlint"            hlint-version       {};
+        lens-family-core = pkgs.haskell.packages.${compiler}.callHackage "lens-family-core" lens-family-version {};
+        lens-family      = pkgs.haskell.packages.${compiler}.callHackage "lens-family"      lens-family-version { inherit lens-family-core; };
+
+        dweSrc = pkgs.fetchFromGitHub {
+          owner  = "xngns";
+          repo   = "dwergaz";
+          rev    = "v0.2.0.4";
+          sha256 = "1cfm1k2683p0aylavgghirajk1zwzvh6wkqq7lf2ssyk26mav2pi";
+        };
         ltpSrc = pkgs.fetchFromGitHub {
           owner  = "xngns";
           repo   = "lens-toml-parser";
-          rev    = "v0.1.0.3";
-          sha256 = "1km96lxc5bjcny5gjccjgcaw05ln7rr9kw1hbvbgpd1rjp0jj23x";
+          rev    = "v0.1.0.4";
+          sha256 = "11xi8gn3zknza87l1plnrgk48z7v7ya18wr45jsr3x5w56ini585";
         };
-        lens-toml-parser = pkgs.haskell.packages.${compiler}.callCabal2nix "lens-toml-parser" ltpSrc {};
-        hecateRaw        = pkgs.haskell.packages.${compiler}.callCabal2nix "hecate" ./. { inherit lens-toml-parser; };
+
+        dwergaz          = pkgs.haskell.packages.${compiler}.callCabal2nix "dwergaz"          dweSrc {};
+        lens-toml-parser = pkgs.haskell.packages.${compiler}.callCabal2nix "lens-toml-parser" ltpSrc { inherit dwergaz hlint lens-family; };
+        hecateRaw        = pkgs.haskell.packages.${compiler}.callCabal2nix "hecate"           ./.    { inherit hlint lens-family lens-toml-parser; };
+
         extDeps          = [ pkgs.sqlite pkgs.gnupg ];
       in
         pkgs.haskell.lib.overrideCabal hecateRaw (oldAttrs: {

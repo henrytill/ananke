8d9015fe79b1ce7b6a27779772ad10fec6e41a4c
Henry Till <henrytill@gmail.com>
2022-10-05 14:40:35 -0700

    release.nix: tweak structure

--- release.nix
+++ release.nix
@@ -1,28 +1,26 @@
 let
   pkgs = import <nixpkgs> {};
-  jobs = {
-    hecate =
-      { compiler ? "ghc924"
-      , doCheck ? ! pkgs.stdenv.isDarwin
-      }:
-      let
-        _hecate = pkgs.haskell.packages.${compiler}.callCabal2nix "hecate" ./. {};
-        extDeps = [ pkgs.gnupg ];
-      in
-        pkgs.haskell.lib.overrideCabal _hecate (oldAttrs: {
-          inherit doCheck;
-          isExecutable            = true;
-          isLibrary               = false;
-          doHaddock               = false;
-          enableLibraryProfiling  = false;
-          enableSharedLibraries   = false;
-          executableSystemDepends = extDeps;
-          testSystemDepends       = extDeps;
-          preCheck = ''
-            export GNUPGHOME="$PWD/example/gnupg"
-          '';
-          postFixup = "rm -rf $out/lib $out/nix-support $out/share/doc";
-        });
-    };
-in
-  jobs
+in {
+  hecate =
+    { compiler ? "ghc924"
+    , doCheck ? ! pkgs.stdenv.isDarwin
+    }:
+    let
+      _hecate = pkgs.haskell.packages.${compiler}.callCabal2nix "hecate" ./. {};
+      extDeps = [ pkgs.gnupg ];
+    in
+      pkgs.haskell.lib.overrideCabal _hecate (oldAttrs: {
+        inherit doCheck;
+        isExecutable            = true;
+        isLibrary               = false;
+        doHaddock               = false;
+        enableLibraryProfiling  = false;
+        enableSharedLibraries   = false;
+        executableSystemDepends = extDeps;
+        testSystemDepends       = extDeps;
+        preCheck = ''
+          export GNUPGHOME="$PWD/example/gnupg"
+        '';
+        postFixup = "rm -rf $out/lib $out/nix-support $out/share/doc";
+      });
+}

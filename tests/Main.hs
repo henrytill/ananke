cfa6cb1a249ea6604c4572bc371bfe88ab0c0def
Henry Till <henrytill@gmail.com>
2022-10-13 21:00:24 -0700

    use Data.Multimap only when -fbackend-json

--- tests/Main.hs
+++ tests/Main.hs
@@ -1,11 +1,17 @@
+{-# LANGUAGE CPP #-}
+
 module Main (main) where
 
 import           Control.Monad       (unless)
 import qualified System.Exit         as Exit
-import qualified Test.Dwergaz        as Dwergaz
 import qualified Test.QuickCheck     as QC
 
+#ifdef BACKEND_JSON
+import qualified Test.Dwergaz        as Dwergaz
+
 import qualified Data.Multimap.Tests as Multimap
+#endif
+
 import qualified Hecate.Properties   as Properties
 
 
@@ -19,9 +25,11 @@ exitOnFalse = flip unless Exit.exitFailure
 
 main :: IO ()
 main = do
+#ifdef BACKEND_JSON
   -- Multimap tests
   let multimapResults = Multimap.runTests
   exitOnFalse $ all Dwergaz.isPassed multimapResults
+#endif
   -- Property tests
   propertyResults <- Properties.doProperties
   exitOnFalse $ all isSuccess propertyResults

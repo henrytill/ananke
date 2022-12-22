8603298efa1a4d7e04e2b7effc54bee45ccd8854
Henry Till <henrytill@gmail.com>
2022-12-21 19:28:09 -0800

    release.nix: tweak formatting

--- release.nix
+++ release.nix
@@ -5,21 +5,21 @@
     , doCheck ? ! pkgs.stdenv.isDarwin
     }:
     let
-      _hecate = pkgs.haskell.packages.${compiler}.callCabal2nix "hecate" ./. {};
+      hecate_ = pkgs.haskell.packages.${compiler}.callCabal2nix "hecate" ./. {};
       extDeps = [ pkgs.gnupg ];
     in
-      pkgs.haskell.lib.overrideCabal _hecate (oldAttrs: {
-        inherit doCheck;
-        isExecutable            = true;
-        isLibrary               = false;
-        doHaddock               = false;
-        enableLibraryProfiling  = false;
-        enableSharedLibraries   = false;
-        executableSystemDepends = extDeps;
-        testSystemDepends       = extDeps;
-        preCheck = ''
-          export GNUPGHOME="$PWD/example/gnupg"
-        '';
-        postFixup = "rm -rf $out/lib $out/nix-support $out/share/doc";
-      });
+    pkgs.haskell.lib.overrideCabal hecate_ (oldAttrs: {
+      inherit doCheck;
+      isExecutable            = true;
+      isLibrary               = false;
+      doHaddock               = false;
+      enableLibraryProfiling  = false;
+      enableSharedLibraries   = false;
+      executableSystemDepends = extDeps;
+      testSystemDepends       = extDeps;
+      preCheck = ''
+        export GNUPGHOME="$PWD/example/gnupg"
+      '';
+      postFixup = "rm -rf $out/lib $out/nix-support $out/share/doc";
+    });
 }

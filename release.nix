9faac3005c3b258d80a58e91aff90768abde503a
Henry Till <henrytill@gmail.com>
2023-01-25 17:43:43 -0800

    release.nix: tweak

--- release.nix
+++ release.nix
@@ -2,21 +2,21 @@
 {
   hecate =
     { compiler ? "ghc943"
-    , doCheck ? ! pkgs.stdenv.isDarwin
+    , doCheck ? true
     }:
     let
-      hecate_ = pkgs.haskell.packages.${compiler}.callCabal2nix "hecate" ./. {};
+      hecate_ = pkgs.haskell.packages.${compiler}.callCabal2nixWithOptions "hecate" ./. "-fbackend-json" {};
       extDeps = [ pkgs.gnupg ];
     in
     pkgs.haskell.lib.overrideCabal hecate_ (oldAttrs: {
       inherit doCheck;
-      isExecutable            = true;
-      isLibrary               = false;
-      doHaddock               = false;
-      enableLibraryProfiling  = false;
-      enableSharedLibraries   = false;
+      isExecutable = true;
+      isLibrary = false;
+      doHaddock = false;
+      enableLibraryProfiling = false;
+      enableSharedLibraries = false;
       executableSystemDepends = extDeps;
-      testSystemDepends       = extDeps;
+      testSystemDepends = extDeps;
       preCheck = ''
         export GNUPGHOME="$PWD/example/gnupg"
       '';

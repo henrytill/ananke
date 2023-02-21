b8b94ae5f37b01cf0d9f6e170cd6dd3585a88ff0
Henry Till <henrytill@gmail.com>
2023-02-21 00:45:52 -0800

    flake.{lock,nix}: update

--- flake.nix
+++ flake.nix
@@ -1,10 +1,16 @@
 {
   description = "A minimal password manager";
 
-  outputs = { self, nixpkgs }:
-    let pkgs = nixpkgs.legacyPackages.x86_64-linux; in
-    {
-      packages.x86_64-linux.hecate  = (import ./release.nix { inherit pkgs; }).hecate {};
-      packages.x86_64-linux.default = self.packages.x86_64-linux.hecate;
-    };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in
+      {
+        packages.hecate = (import ./release.nix { inherit pkgs; }).hecate {};
+        packages.default = self.packages.${system}.hecate;
+      });
 }

e54a41d1f338372257e04e13d271515478f5f1d0
Henry Till <henrytill@gmail.com>
2024-03-25 19:54:30 -0700

    tear it down to the studs

--- ananke.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-export ANANKE_CONFIG_DIR="$(pwd)/example"
-export ANANKE_DATA_DIR="$(pwd)/example"
-export GNUPGHOME="$(pwd)/example/gnupg"
-
-exec cabal v2-run -v0 exe:ananke -- $@

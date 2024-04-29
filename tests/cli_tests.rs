d6c3dcc77ca238219918f741ee1ef3b32b204e54
Henry Till <henrytill@gmail.com>
2024-04-29 13:57:55 -0700

    cli_tests: add import_lookup_single

--- tests/cli_tests.rs
+++ tests/cli_tests.rs
@@ -144,6 +144,27 @@ macro_rules! make_tests {
             }
 
             #[test]
+            fn import_lookup_single() {
+                let path_fixture = PathFixture::mutable_temp().expect("should get path fixture");
+                let dir = path_fixture.path().expect("should get path");
+                copy_config(dir).expect("should copy");
+                let data_file: OsString =
+                    JSON_PATH.into_iter().collect::<PathBuf>().into_os_string();
+                let data_file_str: &str = data_file.to_str().expect("should have path");
+                Command::new(cargo_bin(BIN))
+                    .args(["import", data_file_str])
+                    .envs($vars(dir, dir))
+                    .assert()
+                    .success();
+                Command::new(cargo_bin(BIN))
+                    .args(["lookup", "foomail", "-i", "quux"])
+                    .envs($vars(dir, dir))
+                    .assert()
+                    .stdout_eq(file!("cli_tests/import_lookup_single.stdout"))
+                    .success();
+            }
+
+            #[test]
             fn import_lookup_many() {
                 let path_fixture = PathFixture::mutable_temp().expect("should get path fixture");
                 let dir = path_fixture.path().expect("should get path");

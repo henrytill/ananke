403410ac97c9ff04665b2f249520497e20ac08ac
Henry Till <henrytill@gmail.com>
2024-08-21 14:19:14 -0700

    appease clippy

--- src/data.rs
+++ src/data.rs
@@ -143,9 +143,9 @@ impl FromSql for EntryId {
     }
 }
 
-impl ToString for EntryId {
-    fn to_string(&self) -> String {
-        self.0.to_string()
+impl std::fmt::Display for EntryId {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.0.fmt(f)
     }
 }
 
